ee0d669b63ae6aef2375e8f0fb50ea31308ca379
Uli <murataalievu@gmail.com>
2024-01-10 22:29:30 +0600

    проверка

--- handlers.py
+++ handlers.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from config import TOKEN_API
 from variables import *
 
+scheduler = AsyncIOScheduler()
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 button_help = KeyboardButton('/help')
@@ -19,16 +22,13 @@ async def on_startup(_):
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer('hi, welcome',
-                         reply_markup=kb)
+    await message.answer('hi, welcome', reply_markup=kb)
     await message.delete()
 
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await bot.send_message(chat_id=message.chat.id,
-                           text=HELP_COMMAND,
-                           parse_mode='HTML',
+    await bot.send_message(chat_id=message.chat.id, text=HELP_COMMAND, parse_mode='HTML',
                            reply_markup=ReplyKeyboardRemove())
     await message.delete()
 
@@ -56,32 +56,36 @@ async def check_count(message: types.Message):
 
 
 @dp.message_handler(content_types=['sticker'])
-async def send_stycker_id(message: types.Message):
+async def send_sticker_id(message: types.Message):
     await message.answer(message.sticker.file_id)
     await message.delete()
+
+
+async def send_image_wrapper():
+    message = types.Message(chat=types.Chat(id=1173847253))  # Замените <your_chat_id> на фактический идентификатор вашего чата
+    await send_image(message)
 
 
 @dp.message_handler(commands=['image'])
 async def send_image(message: types.Message):
     await bot.send_photo(chat_id=message.chat.id,
                          photo='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwwwoldi.ru%2F800%2F600%2Fhttps%2Fpbs.twimg.com%2Fmedia%2FCzpLRDAXAAANr9H.jpg&f=1&nofb=1&ipt=c82260aa57cc8f616d0e1880e0695533eb32efb0a75919d82cf1045b89d4d979&ipo=images',
-                         reply_markup=ReplyKeyboardRemove()
-                         )
+                         reply_markup=ReplyKeyboardRemove())
     await message.delete()
 
 
 @dp.message_handler(commands=['location'])
-async def send_locational(message: types.Message):
-    await bot.send_location(chat_id=message.chat.id,
-                            latitude=55,
-                            longitude=74)
+async def send_location(message: types.Message):
+    await bot.send_location(chat_id=message.chat.id, latitude=55, longitude=74)
     await message.delete()
 
 
+scheduler.add_job(send_image_wrapper, trigger='date', run_date=datetime.now() + timedelta(seconds=10))
+scheduler.start()
+
 if __name__ == '__main__':
-    executor.start_polling(dp,
-                           on_startup=on_startup,
-                           skip_updates=True)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+
 """
 Эта функция проверяет считает количество галочек в сообщении пользователя
 """
